b0a73b027985ab2822c1fea6f29dd221f070edcb
David <davidgorski66@gmail.com>
2018-07-02 09:36:09 -0400

    changed pyview to work again, testing on windows

---
+++ main.py
@@ -0,0 +1,38 @@
+import pyview2 as pv
+import operator
+
+# Create a model
+class model:
+    def __init__(self):
+        self.first = 1.0
+        self.second = 2.0
+        self.result = 3.0
+        self.op = 'add'
+
+    def do_it(self):
+        if self.op == 'add':
+            self.result = operator.add(self.first, self.second)
+        elif self.op == 'subtract':
+            self.result = operator.sub(self.first, self.second)
+
+        print('{0} {1} {2} = {3}'.format(self.first, self.op, self.second, self.result))
+        pv.update()
+
+
+# Create a view
+textctrl_first = pv.TextCtrl('first', label='First', dtype=float)
+textctrl_second = pv.TextCtrl('second', label='Second', dtype=float)
+textctrl_result = pv.TextCtrl('result', label='Result', dtype=float)
+
+button_do_it = pv.Button('do_it', label='Do it!')
+combo_op = pv.ComboBox('op', item_list=['add', 'subtract'], label='Operation:')
+
+view = pv.View([[textctrl_first, textctrl_second, textctrl_result],
+                [combo_op],
+                [button_do_it]],
+               title='Small Calculator')
+
+# Run the program
+pv.run(model, view)
+
+
---
+++ pyview2/__init__.py
@@ -0,0 +1,9 @@
+
+# import core functions
+from .widgets import *
+from .mvc import *
+
+# remove external access to internal modules
+del widgets, mvc
+del Figure, FigCanvas, NavigationToolbar, WorkerThread, Thread, matplotlib, np, pub, pylab, wx
+
